7f61b52fd5626375845810e201732078260e4f41
Armando Tello <ckat609@yahoo.com>
2024-02-08 00:54:48 -0600

    add obstructions (buggy)

--- my_funcs.py
+++ my_funcs.py
@@ -3,43 +3,37 @@ import os
 import bmesh
 import mathutils
 import json
+from operator import itemgetter
 
 M = 1000
-wallPrefix = "wall"
-baseboardPrefix = "baseboard"
-floorPrefix = "floor"
-ceilingPrefix = "ceiling"
+
 
 def getJson():
     filepath = bpy.data.filepath
     directory = os.path.dirname(filepath)
     jsonFile = os.path.join(directory, 'test.json')
-    
+
     return jsonFile
-    
 
 
 def getSpaceDocument(jsonFile):
     spaceDocument = {}
-    
+
     with open(jsonFile) as file:
         spaceDocument = json.load(file)
-    
-    return spaceDocument
 
+    return spaceDocument
 
 
 def removeAllObjects():
     for object in bpy.data.objects:
-        bpy.data.objects.remove(object,do_unlink=True,do_id_user=True,do_ui_user=True)
-        
-        
-        
+        bpy.data.objects.remove(object, do_unlink=True,
+                                do_id_user=True, do_ui_user=True)
+
+
 def removeAllCollections():
     for collection in bpy.data.collections:
         bpy.data.collections.remove(collection)
-        
-        
 
 
 def addMeshToObject(vertices, obj):
@@ -51,7 +45,7 @@ def addMeshToObject(vertices, obj):
     bm.verts.ensure_lookup_table()
 
     for idx, v in enumerate(bm.verts):
-        if(idx == len(bm.verts)-1):
+        if (idx == len(bm.verts)-1):
             bm.edges.new([bm.verts[len(bm.verts)-1], bm.verts[0]])
         else:
             bm.edges.new([bm.verts[idx], bm.verts[idx+1]])
@@ -60,92 +54,122 @@ def addMeshToObject(vertices, obj):
 
     bm.to_mesh(objMesh)
     bm.free()
-    
+
     return obj
 
 
+def linkObjectToCollection(obj, collectionName):
 
-def linkObjectToNewCollection(obj, name):
-    newWallCollection = bpy.data.collections.new(name)
-    bpy.context.scene.collection.children.link(newWallCollection)
-    newWallCollection.objects.link(obj)
+    if collectionName not in bpy.data.collections:
+        collection = bpy.data.collections.new(collectionName)
+        bpy.context.scene.collection.children.link(collection)
+
+    bpy.data.collections[collectionName].objects.link(obj)
+    print(bpy.data.collections[collectionName])
 
 
-
-def createCeiling(spaceDocument):
-    name = ceilingPrefix
+def drawCeiling(walls):
+    name = "ceiling"
     emptyMesh = bpy.data.meshes.new(name)
     tempObj = bpy.data.objects.new(name, emptyMesh)
-    
+
     vertices = []
-    for wall in spaceDocument['room']['walls']:
+    for wall in walls:
         points = wall['points']
         points.reverse()
 
         for point in wall['points']:
-            vertex = mathutils.Vector((point['x']/M, point['y']/M, point['z']/M))
+            vertex = mathutils.Vector(
+                (point['x']/M, point['y']/M, point['z']/M))
 
-            if(point['z'] != 0 and vertex not in vertices):
+            if (point['z'] != 0 and vertex not in vertices):
                 vertices.append(mathutils.Vector(vertex))
 
-
     newObj = addMeshToObject(vertices, tempObj)
-    linkObjectToNewCollection(newObj, name)
+    linkObjectToCollection(newObj, name)
 
-    
     return newObj
-        
-        
-    
-def createFloor(spaceDocument):
-    name = floorPrefix
+
+
+def drawFloor(walls):
+    name = "floor"
     emptyMesh = bpy.data.meshes.new(name)
     tempObj = bpy.data.objects.new(name, emptyMesh)
-    
-    vertices = []
-    for wall in spaceDocument['room']['walls']:
-        for point in wall['points']:
-            vertex = mathutils.Vector((point['x']/M, point['y']/M, point['z']/M))
 
-            if(point['z'] == 0 and vertex not in vertices):
+    vertices = []
+    for wall in walls:
+        for point in wall['points']:
+            vertex = mathutils.Vector(
+                (point['x']/M, point['y']/M, point['z']/M))
+
+            if (point['z'] == 0 and vertex not in vertices):
                 vertices.append(mathutils.Vector(vertex))
 
-
     newObj = addMeshToObject(vertices, tempObj)
-    linkObjectToNewCollection(newObj, name)
-    
-    return newObj
-        
-        
-    
-def createWalls(spaceDocument):
+    linkObjectToCollection(newObj, name)
 
-    for wall in spaceDocument['room']['walls']:
-        name = f"{wallPrefix}{wall['id']}"
+    return newObj
+
+
+def drawWalls(walls):
+    for wall in walls:
+        name = f"wall{wall['id']}"
         emptyMesh = bpy.data.meshes.new(name)
         tempObj = bpy.data.objects.new(name, emptyMesh)
 
         vertices = []
         for point in wall['points']:
-            vertices.append(mathutils.Vector((point['x']/M, point['y']/M, point['z']/M)))
+            vertices.append(mathutils.Vector(
+                (point['x']/M, point['y']/M, point['z']/M)))
 
-            
         newObj = addMeshToObject(vertices, tempObj)
-        linkObjectToNewCollection(newObj, name)
-    
- 
+        linkObjectToCollection(newObj, name)
 
 
+def drawObstructions(obstructions):
+    for obstruction in obstructions:
+        elementType = obstruction['type']
+        wallId = obstruction['wallId']
+        x = obstruction['x']/M
+        y = obstruction['y']/M
+        width = obstruction['width']/M
+        height = obstruction['height']/M
+        depth = obstruction['depth']/M
 
+        emptyMesh = bpy.data.meshes.new(elementType)
+        tempObj = bpy.data.objects.new(elementType, emptyMesh)
 
+        verticesBack = [mathutils.Vector((x, 0, y)),  mathutils.Vector(
+            (x+width, 0, y)), mathutils.Vector((x+width,  0, y-height)), mathutils.Vector((x,  0, y-height))]
+        verticesFront = [mathutils.Vector((x, -depth, y)),  mathutils.Vector(
+            (x+width, -depth, y)), mathutils.Vector((x+width,  -depth, y-height)), mathutils.Vector((x,  -depth, y-height))]
+        verticesTop = [mathutils.Vector((x, 0, y)),  mathutils.Vector(
+            (x+width, 0, y)), mathutils.Vector((x, -depth, y)),  mathutils.Vector(
+            (x+width, -depth, y))]
+        verticesBottom = [mathutils.Vector((x+width,  0, y-height)), mathutils.Vector(
+            (x,  0, y-height)), mathutils.Vector((x+width,  -depth, y-height)), mathutils.Vector((x,  -depth, y-height))]
+        verticesLeft = [mathutils.Vector((x, 0, y)), mathutils.Vector(
+            (x,  0, y-height)), mathutils.Vector((x,  -depth, y-height)), mathutils.Vector((x, -depth, y))]
+        verticesRight = [mathutils.Vector((x+width, 0, y)), mathutils.Vector(
+            (x+width,  0, y-height)), mathutils.Vector((x+width,  -depth, y-height)), mathutils.Vector((x+width, -depth, y))]
+        vertices = verticesLeft + verticesRight
+
+        newObj = addMeshToObject(vertices, tempObj)
+        linkObjectToCollection(newObj, f"wall{wallId}")
 
 
 aFile = getJson()
 aSpaceDocument = getSpaceDocument(aFile)
+walls = aSpaceDocument['room']['walls']
+obstructions = aSpaceDocument['room']['obstructions']
 
 removeAllObjects()
 removeAllCollections()
 
-createWalls(aSpaceDocument)
-createFloor(aSpaceDocument)
-createCeiling(aSpaceDocument)
+drawWalls(walls)
+drawFloor(walls)
+drawCeiling(walls)
+drawObstructions(obstructions)
+
+
+# print(f"{obstructions}")
